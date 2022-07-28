9daac9f15dfa2459ec80eb5e450e49f4906adc42
dnzambuli <damunzandm@outlook.com>
2022-07-28 15:07:48 +0300

    basic instructions

---
+++ main.py
@@ -0,0 +1,5 @@
+print('\nAssignment Tracker')
+
+# program isntructions 
+instructions = "\nUse the following key bindings\n1\tA/a - add a new assignment\n2\tD/d - mark as complete\n3\tE/e - end session\n4\tL/l - list all the assignments"
+print(instructions)
