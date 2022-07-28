6d58205e9c1769aa697f72db32d282b54835bf03
dnzambuli <damunzandm@outlook.com>
2022-07-28 15:57:29 +0300

    func to add and create a new assignment

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+import pandas as pd 
+
+# universal store for assignments
+db = './assignments.csv'
+
+# beginning
 print('\nAssignment Tracker')
 
 # program isntructions 
-instructions = "\nUse the following key bindings\n1\tA/a - add a new assignment\n2\tD/d - mark as complete\n3\tE/e - end session\n4\tL/l - list all the assignments"
+instructions = "\nUse the following key bindings\n1\tA/a - add a new assignment\n2\tD/d - mark as complete\n3\tE/e - end session\n4\tL/l - list all the assignments\n\n"
 print(instructions)
+
+def add_assignment(a, b):
+    '''
+    add assignment takes in an assignment a 
+    due date b and adds them to a csv file 
+
+    input: assignment, due date
+    output: 0 pass, 1 Fail
+
+    return: int
+    '''
+    # how data is to be stored in the csv file
+    data = pd.dataframe({'assignments': a, 'due date': b})
+
+    try:
+        df.to_csv(db)
+        return 0
+    except:
+        print("invalid process adding new assignment")
+        return 1
+    
+def more_assignments(a, b):
+    '''
+    add more assignments a 
+    due date b and adds them to a csv file 
+
+    input: assignment, due date
+    output: 0 pass, 1 Fail
+
+    return: int
+    '''
+    # the currently saved data
+    
+    new_assignments = pd.dataframe({'assignments': a, 'due date': b})
+    
